8226a1acacb9ecd197e167844fe61df296d3c9f8
lucien1011 <lucien1011@gmail.com>
2015-08-14 11:01:54 +0100

    added run lumi and event number option

--- analyze_MET_cfg.py
+++ analyze_MET_cfg.py
@@ -6,9 +6,18 @@ import ROOT
 ROOT.gROOT.SetBatch(1)
 ROOT.gROOT.ProcessLine( "gErrorIgnoreLevel = 1001;")
 
+parser.add_option("-r","--run", action = "store", default = 1, type = 'long',help ="run number")
+parser.add_option("-l","--lumi", action = "store", default = 1, type = 'long',help="lumi section")
+parser.add_option("-e","--evt", action = "store", default = 1, type = 'long',help="event number")
+
+(options,args) = parser.parse_args(sys.argv)
+
+run = options.run
+ls = options.lumi
+evt = options.evt
 inputPath = options.inputPath
 
 analyzer = MiniAODAnalyzer()
 analyzer.loadFiles(inputPath)
-analyzer.whichEvent(1,2433,319961)
+analyzer.whichEvent(run,ls,evt)
 analyzer.loop()
