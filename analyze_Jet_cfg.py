dd57700cb4edd0cd249e24dac8d2e35cd6aeeb07
lucien1011 <lucien1011@gmail.com>
2015-08-15 21:39:21 +0100

    make jet cfg to work

--- analyze_Jet_cfg.py
+++ analyze_Jet_cfg.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
-from Core import *
+from Core.Sequence import Sequence
+from Core.EventLooper import EventLooper
+from Core.InputParser import parser
+import sys
 from Analyzer.JetAnalyzer import JetAnalyzer
 from Utils.DBHandler import getDBPath
 
@@ -18,6 +21,10 @@ datasetName = options.dataset
 inputFilePath = getDBPath(datasetName,run,ls)
 
 jetAna = JetAnalyzer()
-jetAna.loadFiles(inputFilePath)
 jetAna.whichEvent(run,ls,evt)
-jetAna.loop()
+
+sequence = Sequence()
+sequence.load(jetAna)
+
+looper = EventLooper(sequence,inputFilePath)
+looper.loop()
