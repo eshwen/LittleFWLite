30d3151911616bba517e45e30b980f81bc162d2a
lucien1011 <lucien1011@gmail.com>
2015-08-15 20:30:14 +0100

    updated example

--- example.sh
+++ example.sh
@@ -1,4 +1,2 @@
 
-das_client.py --query="file dataset=/QCD_Pt_120to170_TuneCUETP8M1_13TeV_pythia8/RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v1/MINIAODSIM run=1 lumi=301" --limit 0
-./analyze_MET_cfg.py --inputPath=root://gfe02.grid.hep.ph.ic.ac.uk/pnfs/hep.ph.ic.ac.uk/data/cms/store/mc/RunIISpring15DR74/QCD_Pt_120to170_TuneCUETP8M1_13TeV_pythia8/MINIAODSIM/Asympt50ns_MCRUN2_74_V9A-v1/70000/DC5650CD-FFFA-E411-87EE-E0CB4E19F95B.root --run=1 --lumi=301 --evt=49969
-
+./analyze_MET_cfg.py --dataset="/QCD_Pt_120to170_TuneCUETP8M1_13TeV_pythia8/RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v1/MINIAODSIM" --run=1 --lumi=5920 --evt=984583
