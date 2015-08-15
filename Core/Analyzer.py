581645b5ea75c370b46d12bc9210ed202331cafa
lucien1011 <lucien1011@gmail.com>
2015-08-15 21:24:15 +0100

    separate Analyzer with EventLooper

---
+++ Core/Analyzer.py
@@ -0,0 +1,19 @@
+
+class Analyzer(object):
+	def __init__(self):
+		pass
+
+	def beginJob(self):
+		pass
+
+	def declareHandles(self):
+		pass
+
+	def analyze(self,event):
+		pass
+
+	def endJob(self):
+		pass
+
+	def applySelection(self,event):
+		pass
--- Core/EventLooper.py
+++ Core/EventLooper.py
@@ -3,36 +3,23 @@ import ROOT
 from LoadLibraries import *
 
 class EventLooper(object):
-	def __init__(self):
-		# self.inputPath = [inputPath]
-		# self.nEvent
-		pass
+	def __init__(self,sequence,inputPath=None):
+		self.sequence = sequence		
+		if inputPath:
+			self.file = ROOT.TFile.Open(inputPath)
 
 	def loadFiles(self,inputPath):
 		self.file = ROOT.TFile.Open(inputPath)
 
-	def declareHandles(self):
-		pass
-
 	def loop(self,nEvents = -1):
 		self.events = Events(self.file,maxEvents=nEvents)
-		self.declareHandles()
-		self.beginJob()
+		for ana in self.sequence:
+			ana.beginJob()
 		for i,event in enumerate(self.events):
 			if (i > nEvents) and (nEvents != -1): break
-			if not  self.applySelection(event): continue
-			self.analyze(event)
-		self.endJob()
-
-	def analyze(self,event):
-		pass
-
-	def applySelection(self,event):
-		pass
-
-	def beginJob(self):
-		pass
-
-	def endJob(self):
-		pass
+			for ana in self.sequence:
+				if not  ana.applySelection(event): continue
+				ana.analyze(event)
+		for ana in self.sequence:
+			ana.endJob()
 	
