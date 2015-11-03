cd481fc8ec687f11ab2523ef11fb4c2c3bab8090
lucien1011 <lucien1011@gmail.com>
2015-11-03 17:48:24 +0100

    support cern DB

--- Utils/DBHandler.py
+++ Utils/DBHandler.py
@@ -8,8 +8,11 @@ def getDBPath(PDName,run,lumi):
 
 	if whereAmI() == "Imperial":
 		return "root://gfe02.grid.hep.ph.ic.ac.uk/pnfs/hep.ph.ic.ac.uk/data/cms"+process.communicate()[0].split("\n")[0]
+	elif whereAmI() == "CERN":
+		return "root://eoscms//eos/cms"+process.communicate()[0].split("\n")[0]
+
 	else:
-		raise RuntimeError, "getDBPath function only supports IC for the moment."
+		raise RuntimeError, "getDBPath function only supports IC and CERN for the moment."
 
 def getFilesFromPD(PDName):
 	cmdList = ["das_client.py","--query","file dataset=%s"%(PDName),"--limit","0"]
@@ -17,6 +20,8 @@ def getFilesFromPD(PDName):
 	
 	if whereAmI() == "Imperial":
 		return ["root://gfe02.grid.hep.ph.ic.ac.uk/pnfs/hep.ph.ic.ac.uk/data/cms"+fileName for fileName in process.communicate()[0].split("\n") if fileName.endswith(".root")]
+	elif whereAmI() == "CERN":
+		return ["root://eoscms//eos/cms"+fileName for fileName in process.communicate()[0].split("\n") if fileName.endswith(".root")]
 	else:
 		raise RuntimeError, "getFilesFromPD function only supports IC for the moment."
 
