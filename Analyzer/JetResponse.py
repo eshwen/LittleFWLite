9f610a94399a1b677cc1cff3987e5ebb428d6b30
lucien1011 <lucien1011@gmail.com>
2015-11-30 17:07:54 +0000

    added JetResponse Analyzer

---
+++ Analyzer/JetResponse.py
@@ -0,0 +1,38 @@
+
+from Analyzer.JetAnalyzer import JetAnalyzer
+from DataFormats.FWLite import Handle 
+import math
+import ROOT
+from Utils.MatchCollections import matchObjectCollection
+
+class JetResponse(JetAnalyzer):
+	def beginJob(self):
+		super(JetResponse,self).beginJob()
+
+        #____________________________________________________________________________||
+
+        if not hasattr(self,"outFilePath"):
+            outFilePath = "jetResponse.root"
+
+        self.outFile = ROOT.TFile(outFilePath,"RECREATE")i
+        self.hists["jetResponse"] = ROOT.TH1D("JetResponse"," ; Jet Response ;  ",50,0.5,2.)
+        self.hists["recoJetPt_vs_genJetPt"] = ROOT.TH1D("recoJetPt_vs_genJetPt"," ; RECO Jet p_{T} ; GEN Jet p_{T}  ",50,0.,100.,50,0.,100.)
+
+        #____________________________________________________________________________||
+
+	def analyze(self,event):
+
+        #____________________________________________________________________________||
+
+        super(JetResponse,self).analyze( event )
+
+        #____________________________________________________________________________||
+        
+        pairs = matchObjectCollection( pfJets , genJets , 0.3 ) 
+        
+        for pfJet in pfJets:
+            if pfJet.matched:
+                matchGenJet = pairs[pfJet]
+                self.hists["jetResponse"].Fill( pfJet.pt() / matchGenJet.pt() )
+                self.hists["recoJetPt_vs_genJetPt"].Fill( pfJet.pt() , matchGenJet.pt() )
+        
