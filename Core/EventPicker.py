52f9b2a35952d4ba9cc1cba13fcf6eefb245d51e
lucien1011 <lucien1011@gmail.com>
2015-11-30 16:15:54 +0000

    print progress

--- Core/EventPicker.py
+++ Core/EventPicker.py
@@ -3,43 +3,47 @@ import ROOT
 from DataFormats.FWLite import Events
 
 class EventPicker(object):
-	def __init__(self,sequence,inputPath=None):
-		self.sequence = sequence		
-		if inputPath:
-			self.inputPath = inputPath
-		self.eventList = []
+    def __init__(self,sequence,inputPath=None):
+        self.sequence = sequence		
+        if inputPath:
+            self.inputPath = inputPath
+        self.eventList = []
 
-	def addEventList(self,selectRun,selectLumi,selectEvent):
-		self.eventList.append((selectRun,selectLumi,selectEvent))
+    def addEventList(self,selectRun,selectLumi,selectEvent):
+        self.eventList.append((selectRun,selectLumi,selectEvent))
 
-	def loop(self,nEvents = -1):
-		if not self.eventList:
-			raise RuntimeError,"Run Number or Lumi Section or Event number not specified."
-		if type(self.inputPath) != list:
-			self.inputPath = [self.inputPath]
-		for filePath in self.inputPath:
-			file = ROOT.TFile.Open(filePath)
-			# print filePath
-			events = Events(file,maxEvents=nEvents)
-			for ana in self.sequence:
-				ana.beginJob()
-			for i,event in enumerate(events):
-				if (i > nEvents) and (nEvents > 0): break
-				if not self.isSelectEvent(event): continue
-				for ana in self.sequence:
-					if not ana.applySelection(event): continue
-					ana.analyze(event)
-			for ana in self.sequence:
-				ana.endJob()
-			file.Close()
+    def loop(self,nEvents = -1):
+        if not self.eventList:
+            raise RuntimeError,"Run Number or Lumi Section or Event number not specified."
+        if type(self.inputPath) != list:
+            self.inputPath = [self.inputPath]
+        for filePath in self.inputPath:
+            file = ROOT.TFile.Open(filePath)
+            if self.printProcess:
+                print "Processing file: %s"%filePath
+            # print filePath
+            events = Events(file,maxEvents=nEvents)
+            for ana in self.sequence:
+                ana.beginJob()
+            for i,event in enumerate(events):
+                if self.printProcess and i % 10000 == 0:
+                    print "%s events processed"%i
+                if (i > nEvents) and (nEvents > 0): break
+                if not self.isSelectEvent(event): continue
+                for ana in self.sequence:
+                    if not ana.applySelection(event): continue
+                    ana.analyze(event)
+            for ana in self.sequence:
+                ana.endJob()
+            file.Close()
 
 
-	def isSelectEvent(self,event):
-		run = event.eventAuxiliary().run()
-		lumi = event.eventAuxiliary().luminosityBlock()
-		eventId = event.eventAuxiliary().event()
+    def isSelectEvent(self,event):
+        run = event.eventAuxiliary().run()
+        lumi = event.eventAuxiliary().luminosityBlock()
+        eventId = event.eventAuxiliary().event()
 
-		return any([run == evtList[0] and lumi == evtList[1] and eventId == evtList[2] for evtList in self.eventList]) 
-		# return any([run == evtList[0] and lumi == evtList[1] and eventId == evtList[2] for evtList in self.eventList]) 
-		
-		
+        return any([run == evtList[0] and lumi == evtList[1] and eventId == evtList[2] for evtList in self.eventList]) 
+    # return any([run == evtList[0] and lumi == evtList[1] and eventId == evtList[2] for evtList in self.eventList]) 
+
+
--- pickEvents_Jet_cfg.py
+++ pickEvents_Jet_cfg.py
@@ -9,11 +9,13 @@ from Utils.TextFileHandler import ReadEventList
 
 parser.add_option("-p","--dataset", action = "store", default = 1, type = 'string',help="Primary Dataset")
 parser.add_option("-t","--textPath", action = "store", default = "file.txt", type = 'string',help="Event List Text File")
+parser.add_option("--printProcess", action = "store_true",default = False)
 
 (options,args) = parser.parse_args(sys.argv)
 
 datasetName = options.dataset
 textPath = options.textPath
+printProcess = options.printProcess
 
 inputFilePath = getFilesFromPD(datasetName)
 eventList = ReadEventList(textPath) 
@@ -24,5 +26,6 @@ sequence = Sequence()
 sequence.load(jetAna)
 
 looper = EventPicker(sequence,inputFilePath)
+EventPicker.printProcess = printProcess
 looper.eventList = eventList
 looper.loop()
