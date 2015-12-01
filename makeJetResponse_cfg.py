d99be1b702a79e17f90c7b2eb6335320183efa5d
lucien1011 <lucien1011@gmail.com>
2015-12-01 15:54:49 +0000

    handle input file lists and text file

--- makeJetResponse_cfg.py
+++ makeJetResponse_cfg.py
@@ -5,8 +5,8 @@ from Analyzer.JetResponse import JetResponse
 
 #____________________________________________________________________________||
 
-parser.add_option("-p","--dataset", action = "store", default = 1, type = 'string',help="Primary Dataset")
-parser.add_option("-t","--textPath", action = "store", default = "file.txt", type = 'string',help="Event List Text File")
+parser.add_option("-p","--dataset", action = "store", type = 'string',help="Primary Dataset")
+parser.add_option("-t","--textPath", action = "store", type = 'string',help="Event List Text File")
 
 #____________________________________________________________________________||
 
@@ -15,13 +15,18 @@ parser.add_option("-t","--textPath", action = "store", default = "file.txt", typ
 datasetName = options.dataset
 textPath = options.textPath
 nevents = options.nevents
+inputPath = options.inputPath
 
 #____________________________________________________________________________||
 
-eventList = ReadEventList( textPath )
-#inputFileList = [ getDBPath( datasetName , run , lumi )  for (run,lumi,event) in eventList ]
+if inputPath:
+    inputFileList = [ '/vols/cms04/lucienlo/output/01_12_2015/SkimMiniAOD/QCD500To700_skim.root' ]
+elif textPath and datasetName:
+    eventList = ReadEventList( textPath )
+    inputFileList = [ getDBPath( datasetName , run , lumi )  for (run,lumi,event) in eventList ]
+else:
+    raise RuntimeError,"Input files not correctly specifed"
 
-inputFileList = [ 'root://gfe02.grid.hep.ph.ic.ac.uk/pnfs/hep.ph.ic.ac.uk/data/cms/store/mc/RunIISpring15DR74/QCD_HT500to700_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v1/00000/32ACD783-A818-E511-80D1-000F530E4798.root' ]
 
 #____________________________________________________________________________||
 
